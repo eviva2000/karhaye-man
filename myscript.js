c72d56b68107ab925913ffb98ab9ae327854fcfd
elahe ar <elahe@Brugers-MacBook-Pro-2.local>
2018-10-06 23:46:26 +0200

    working on about section and changing bg img of projects

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 let buble = document.querySelector("#buble");
 let image = document.querySelector(".image");
 setTimeout(function() {
-  document.getElementById("buble").style.display = "block";
-  buble.classList.add("buble-pop");
+  // document.getElementById("buble").style.display = "block";
+  // buble.classList.add("buble-pop");
   console.log("i m working");
 }, 1000);
 
